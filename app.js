63c6c0303366027322de5335ad61eda8e317ad31
millord <juancarlosmillord@gmail.com>
2021-02-25 16:01:58 -0400

    working now

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 const path = require('path')
 
+
 const app = express();
 
 app.use(cors())
@@ -36,7 +37,29 @@ app.post('/create', (req, res) =>{
   console.log(req.body.templateData)
   content = req.body.templateData
   var fs = require('fs')
-fs.appendFile('./public/css/styles.css', req.body.templateData.css, function (err) {
+fs.writeFile('./public/css/styles.css', req.body.templateData.css, function (err) {
+  if (err) {
+    // append failed
+  } else {
+    // done
+  }
+})
+fs.writeFile('./template.html', 
+  `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+    ${req.body.templateData.css}
+    </style>
+  </head>
+  <body>
+    ${req.body.templateData.html}
+  </body>
+  </html>`, function (err) {
   if (err) {
     // append failed
   } else {
